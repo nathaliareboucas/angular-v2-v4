2bcb9da5372ce476e252106a6b96df0227910f7e
Nathália Rebouças <nathreboucas5@gmail.com>
2018-01-16 07:36:20 -0300

    Forms (template driven) Inicializando valores com ngModel (two-way data-binding)

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -1,6 +1,7 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule} from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,6 +16,8 @@ import { DataFormComponent } from './data-form/data-form.component';
     DataFormComponent
   ],
   imports: [
+    FormsModule,
+    HttpModule,
     BrowserModule,
     AppRoutingModule
   ],
